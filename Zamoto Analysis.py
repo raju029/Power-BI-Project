2bfd09f723d4b9a8b496ae7c27bcf767bbc9cffc
raju029 <rsmtr8923@gmail.com>
2024-09-22 01:01:45 +0530

    Update Zamoto Analysis.py

--- Zamoto Analysis.py
+++ Zamoto Analysis.py
@@ -5,13 +5,13 @@ import pandas as pd
 import numpy as np
 
 # Loading .csv file of zomato data using pandas
-df_data = pd.read_csv(r"Zamoto raw data\zomato.csv", encoding='unicode_escape')
+df_data = pd.read_csv(r"Raw Zomato data\zomato.csv", encoding='unicode_escape')
 print(df_data)
 
 
 # Loading excel file of country code using pandas
 
-df_country = pd.read_excel(r"Zamoto raw data\Country-Code.xlsx")
+df_country = pd.read_excel(r"Raw Zomato data\Country-Code.xlsx")
 print(df_country)
 
 
